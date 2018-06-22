719be2bcd8f87b06ae5b13aa5c5988c0d36d3b22
Agnieszka Jacek <agnieszka.jacek90@gmail.com>
2018-06-22 16:33:39 +0200

    Set cookies and hide Log in button

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,6 +9,7 @@ class PostsController < ApplicationController
       @category_id = Category.find_by(name: params[:category])
       @posts = Post.where(category_id: @category_id).order("created_at DESC")
     end
+
   end
 
   def show
