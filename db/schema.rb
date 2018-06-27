777b53a1442c6db3e2f208087741c6d19b75d4fc
Agnieszka Jacek <agnieszka.jacek90@gmail.com>
2018-06-27 22:05:26 +0200

    Change sql to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20180626202807) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -44,7 +47,7 @@ ActiveRecord::Schema.define(version: 20180626202807) do
   end
 
   create_table "subcategories", force: :cascade do |t|
-    t.integer "category_id"
+    t.bigint "category_id"
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -69,4 +72,5 @@ ActiveRecord::Schema.define(version: 20180626202807) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "subcategories", "categories"
 end
