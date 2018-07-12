f725f4a69a006e74771dbdce10b6753e07a431bf
Agnieszka Jacek <agnieszka.jacek90@gmail.com>
2018-07-12 20:02:03 +0200

    Add shadow to post container Create base views for Category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,8 +1,56 @@
 class CategoriesController < ApplicationController
+  before_action :find_category, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
-  
+
+  def index
+    @categories = Category.all.order("created_at DESC")
+  end
+
   def show
     @category = Category.friendly.find(params[:id])
     @posts = Post.where(category_id: @category).order("created_at DESC")
+  end
+
+  def new
+    parent_category
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save!
+      redirect_to @category, notice: "The category was created!"
+    else
+      render "new"
+    end
+  end
+
+  def edit
+    parent_category
+  end
+
+  def update
+    if @category.update(category_params)
+      redirect_to @category, notice: "Update successful!"
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+     @category.destroy
+     redirect_to root_path, notice: "Category destroyed"
+  end
+
+  private
+  def category_params
+    params.require(:category).permit(:name, :parent_id)
+  end
+
+  def find_category
+    @category = Category.friendly.find(params[:id])
+  end
+  def parent_category
+    @parent_category = Category.where(ancestry: nil)
   end
 end
