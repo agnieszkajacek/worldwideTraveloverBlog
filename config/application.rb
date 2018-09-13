c2b2f8e14c96ed4ff63d6ad6e24109179f9087ba
Agnieszka Jacek <agnieszka.jacek90@gmail.com>
2018-09-13 19:28:11 +0200

    Add fonts

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,7 @@ module Blog
     config.action_mailer.sendgrid_actionmailer_settings = {
       api_key: ENV['SENDGRID_API_KEY']
     }
+    config.assets.paths << Rails.root.join("app", "assets", "fonts")
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
