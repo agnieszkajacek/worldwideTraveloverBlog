b439c7567f786e7358a3522149e5e042e354f7fb
agnieszka <agnieszka.jacek90@gmail.com>
2018-12-09 20:19:45 +0100

    Change max size in ImageUploader

--- app/models/image_uploader.rb
+++ app/models/image_uploader.rb
@@ -9,7 +9,7 @@ class ImageUploader < Shrine
   plugin :pretty_location
 
   Attacher.validate do
-    validate_max_size 5.megabytes, message: 'is too large (max is 5 MB)'
+    validate_max_size 5.megabytes, message: 'is too large (max is 13 MB)'
     validate_mime_type_inclusion %w[image/jpg image/jpeg image/png image/gif]
   end
 
