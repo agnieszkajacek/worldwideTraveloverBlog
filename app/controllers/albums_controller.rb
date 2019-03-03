752b81eb145a0e4171f21b83bd79221ee8531804
agnieszka <agnieszka.jacek90@gmail.com>
2019-03-03 21:57:23 +0100

    Change showing photos in albums - admin can see all photos but the rest user only public photos

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -3,7 +3,12 @@ class AlbumsController < ApplicationController
   
   def show
     @category = Category.friendly.find(params[:id])
-    @photos = Photo.where(category_id: @category, public: true)
+    @photos = @photos = Photo.where(category_id: @category)
+
+    if (!user_signed_in?)
+      @photos = Photo.where(category_id: @category, public: true)
+    end
+    
     @uniq_tags = @photos.map { |photo|  photo.tag}.uniq.reject(&:blank?)
 
     @tags = @uniq_tags.map do |tag|
