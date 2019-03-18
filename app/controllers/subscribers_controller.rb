e74444f0d6f98165de7e25ad83ed55649b464d1c
Agnieszka Jacek <agnieszka.jacek90@gmail.com>
2019-03-18 17:00:42 +0100

    Add reCAPTCHA

--- app/controllers/subscribers_controller.rb
+++ app/controllers/subscribers_controller.rb
@@ -3,10 +3,13 @@ class SubscribersController < ApplicationController
     @subscriber = Subscriber.new
   end
   def create
+
     active_subscriber = Subscriber.where("email = ? AND subscription = ?", params[:subscriber][:email], true)
     unactive_subscriber = Subscriber.where("email = ? AND subscription = ?", params[:subscriber][:email], false)
 
-    if active_subscriber.exists?
+    if !is_user_a_human?
+      redirect_to root_path, notice: 'Wygląda na to, że jesteś botem :('
+    elsif active_subscriber.exists?
       redirect_to root_path, notice: 'Wygląda na to, że już ze mną jesteś na bieżąco :)'
     elsif unactive_subscriber.exists?
       subscriber = unactive_subscriber.first
@@ -16,7 +19,7 @@ class SubscribersController < ApplicationController
     else
       @subscriber = Subscriber.create(subscriber_params)
       if @subscriber.save!
-        cookies[:saved_subscriber] = true
+        #cookies[:saved_subscriber] = true
         NotificationMailer.with(subscriber: @subscriber).welcome_email.deliver_now
         redirect_to root_path, notice: 'Hura! Udało Ci się zapisać do newslettera!'
       end
@@ -31,7 +34,17 @@ class SubscribersController < ApplicationController
   end
 
   private
+
+  def is_user_a_human?
+    response = HTTP.post("https://www.google.com/recaptcha/api/siteverify", params: {
+      secret: ENV["RECAPTCHA_PRIVATE_KEY"],
+      response: subscriber_params[:recaptcha_token]
+    }).parse
+
+    response["success"] && response["score"] >= 0.5
+  end
+
   def subscriber_params
-    params.require(:subscriber).permit(:email)
+    params.require(:subscriber).permit(:email, :recaptcha_token)
   end
 end
--- app/models/subscriber.rb
+++ app/models/subscriber.rb
@@ -2,6 +2,8 @@ class Subscriber < ApplicationRecord
   validates :email, presence: true
   before_create :add_unsubscribe_hash
 
+  attr_accessor :recaptcha_token
+  
   private
   def add_unsubscribe_hash
     self.unsubscribe_hash = SecureRandom.hex
