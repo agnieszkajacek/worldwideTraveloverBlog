8d8938616f910ff765c9a43db8a3d5f533bdb306
Agnieszka Jacek <agnieszka.jacek90@gmail.com>
2019-03-21 15:52:35 +0100

    Change path for sitemap

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -47,5 +47,5 @@ SitemapGenerator::Sitemap.adapter = SitemapGenerator::AwsSdkAdapter.new(
 )
 
 SitemapGenerator::Sitemap.public_path = 'tmp/'
-SitemapGenerator::Sitemap.sitemaps_host = "https://<%= ENV['AWS_BUCKET'] %>.s3.amazonaws.com/"
+SitemapGenerator::Sitemap.sitemaps_host = "http://s3.amazonaws.com/<%= ENV['AWS_BUCKET'] %>"
 SitemapGenerator::Sitemap.sitemaps_path = 'sitemaps/'
