5618b56ea06c1f42fb41353fcae014aca4550938
Agnieszka Jacek <agnieszka.jacek90@gmail.com>
2019-04-12 20:15:15 +0200

    Redirect old routes to new paths

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,21 @@ Rails.application.routes.draw do
 
   get '/sitemap.xml', to: redirect("https://s3.#{ENV['AWS_REGION']}.amazonaws.com/#{ENV['AWS_BUCKET']}/sitemaps/sitemaps/sitemap.xml.gz", status: 301)
 
+  #get '/pl/tags/europe.html'
+  get '/en/gallery/malta.html', to: redirect('/albums/malta')
+  #get '/en/tags/thailand.html', to: redirect('/categories/tajlandia')
+  #get '/en/tags/bangkok.html'
+  #get '/pl/tags/tenerife.html'
+  get '/pl/tags/poland.html', to: redirect('/categories/polska')
+  get '/pl/tags/italy.html', to: redirect('/categories/wlochy')
+  #get '/en/tags/kazimierz_dolny.html', to: redirect('/posts/cudze-chwalicie-swego-nie-znacie-w-roli-glownej-kazimierz-dolny')
+  get '/pl/tags/bergamo-milano.html', to: redirect('/posts/bergamo-i-milano')
+  get '/pl/2017/04/19/chiang-mai.html', to: redirect('/posts/chiang-mai')
+  get '/pl/2017/04/13/tajlandia-garsc-niezbednych-informacji.html', to: redirect('/posts/tajlandia-garsc-niezbednych-informacji')
+  #get '/en/2017/04/13/thailand-hadful-of-necessary-information.html', to: redirect('/posts/tajlandia-garsc-niezbednych-informacji')
+  #get '/en/2017/05/05/bangkok-city-of-angels.html', to: redirect('/posts/bangkok-miasto-aniolow')
+  get '/pl/2017/03/27/malta-w-listopadzie-podstawowe-informacje.html', to: redirect('/posts/malta-w-listopadzie-podstawowe-informacje')
+
   resources :posts
   resources :albums, only: [] do
     collection do
