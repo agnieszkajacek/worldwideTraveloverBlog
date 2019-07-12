22066e358834be5a65e2466b6b8aa96045321118
Agnieszka Jacek <agnieszka.jacek90@gmail.com>
2019-07-12 22:56:56 +0200

    Create helper which replace html in post to show photo in lightbox gallery or allow to download image

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,6 +13,26 @@ module ApplicationHelper
     return markdown.render(text).html_safe
   end
 
+  def replace_photos(text)
+    # .to_str is needed because ActiveSupport::SafeBuffer breaks assignment to $1 in gsub block
+    # https://github.com/rails/rails/commit/e05d4cea69919ed0a2e5832bde120b5d0f12c0ec#diff-f639a79f308e72f54af369291a4d5907
+    text.to_str.gsub(/\{\{(show_lightbox|download):([0-9]+)\}\}/) do |match|
+      photo = Photo.find_by(id: $2)
+      if photo
+        data = $1 == 'show_lightbox' ? {gallery: "gallery-name", toggle: "lightbox", footer: photo.name} : {}
+        tag.div(class: 'text-center image-style') do
+          tag.div(class: 'image') do
+            link_to photo.image_url(:original, response_content_disposition: 'attachment'), data: data, download: true do
+              tag.img(class: 'inline-image', src: photo.image_url(:original, public: true))
+            end
+          end
+        end
+      else 
+        ""
+      end
+    end.html_safe
+  end
+
   def render_source args={}
     @html_encoder ||= HTMLEntities.new
     raw(@html_encoder.encode(render args))
