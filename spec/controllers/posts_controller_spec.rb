91a1b9d8e03e1f596dd5174f8e84a4ec65f76c7d
Agnieszka Jacek <agnieszka.jacek90@gmail.com>
2019-08-08 13:42:51 +0200

    Check cases when user is not logged_in

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -63,140 +63,179 @@ RSpec.describe PostsController do
   end
 
   describe 'GET #new' do
-    login_user
-
-    it 'creates new post and assigns it to @post' do
-      get :new
-      expect(assigns(:post)).to be_a_new(Post)
+    context 'when user is logged_in' do
+      login_user
+  
+      it 'creates new post and assigns it to @post' do
+        get :new
+        expect(assigns(:post)).to be_a_new(Post)
+      end
+  
+      it 'renders the :new template' do
+        get :new 
+        expect(response).to render_template('new')
+      end
+  
+      it "has a 200 status code" do
+        get :new
+        expect(response.status).to eq(200)
+      end
     end
 
-    it 'renders the :new template' do
-      get :new 
-      expect(response).to render_template('new')
+    context 'when user is not logged_in' do
+      it 'redirects to user_session_path' do
+        get :new
+      
+        expect(response.status).to eq(302)
+        expect(response).to redirect_to(user_session_path)
+      end
     end
-
-    it "has a 200 status code" do
-      get :new
-      expect(response.status).to eq(200)
-    end
-  end
-
-  describe 'GET #edit' do
-    login_user
   end
 
   describe 'POST #create' do
-    login_user
-
-    context 'with valid attributes' do
-      let!(:category) { create(:category) }
-      let!(:new_post) { attributes_for(:post, category_id: category) }
-
-      it 'saves the new post in the database' do
-        expect {
-          post :create, params: { post: new_post }
-        }.to change(Post, :count).by(1)
-      end
-
-      it 'sends email notification to subscribers' do
-        subscribers = 2.times.map do
-          create(:subscriber, subscription: true)
+    context 'when user is logged_in' do
+      login_user
+  
+      context 'with valid attributes' do
+        let!(:category) { create(:category) }
+        let!(:new_post) { attributes_for(:post, category_id: category) }
+  
+        it 'saves the new post in the database' do
+          expect {
+            post :create, params: { post: new_post }
+          }.to change(Post, :count).by(1)
         end
-        
-        fake_mail = double
-        expect(fake_mail).to receive(:deliver_later).with(wait_until: be_within(1.second).of(Time.zone.now))
-        expect(NotificationMailer).to receive(:post_email).with(subscribers[0], kind_of(Post)).once.and_return(fake_mail)
-
-        fake_mail_2 = double
-        expect(fake_mail_2).to receive(:deliver_later).with(wait_until: be_within(1.second).of(Time.zone.now + 15.minutes))
-
-        expect(NotificationMailer).to receive(:post_email).with(subscribers[1], kind_of(Post)).once.and_return(fake_mail_2)
-        
-        post :create, params: { post: new_post }
+  
+        it 'sends email notification to subscribers' do
+          subscribers = 2.times.map do
+            create(:subscriber, subscription: true)
+          end
+          
+          fake_mail = double
+          expect(fake_mail).to receive(:deliver_later).with(wait_until: be_within(1.second).of(Time.zone.now))
+          expect(NotificationMailer).to receive(:post_email).with(subscribers[0], kind_of(Post)).once.and_return(fake_mail)
+  
+          fake_mail_2 = double
+          expect(fake_mail_2).to receive(:deliver_later).with(wait_until: be_within(1.second).of(Time.zone.now + 15.minutes))
+  
+          expect(NotificationMailer).to receive(:post_email).with(subscribers[1], kind_of(Post)).once.and_return(fake_mail_2)
+          
+          post :create, params: { post: new_post }
+        end
+  
+        it 'redirects to the post page' do
+          post :create, params: { post: new_post }
+          expect(response).to redirect_to(Post.last)
+          expect(response.status).to eq(302)
+        end
       end
 
-      it 'redirects to the post page' do
-        post :create, params: { post: new_post }
-        expect(response).to redirect_to(Post.last)
-        expect(response.status).to eq(302)
+      context 'with invalid attributes' do
+        let!(:category) { create(:category) }
+        let!(:new_post) { attributes_for(:post, category_id: nil) }
+        
+        it 'does not save the new post in the database' do
+          expect {
+            post :create, params: { post: new_post }
+          }.not_to change(Post, :count)
+        end
+    
+        it 're-renders the :new template' do
+          post :create, params: { post: new_post }
+          expect(response).to render_template('new')
+        end
       end
     end
-    
-    context 'with invalid attributes' do
-      let!(:category) { create(:category) }
-      let!(:new_post) { attributes_for(:post, category_id: nil) }
-      
-      it 'does not save the new post in the database' do
-        expect {
-          post :create, params: { post: new_post }
-        }.not_to change(Post, :count)
-      end
 
-      it 're-renders the :new template' do
-        post :create, params: { post: new_post }
-        expect(response).to render_template('new')
+    context 'when user is not logged_in' do
+      it 'redirects to user_session_path' do
+        post :create, params: { post: attributes_for(:post) }
+
+        expect(response.status).to eq(302)
+        expect(response).to redirect_to(user_session_path)
       end
     end
   end
-
+    
   describe 'PUT #update' do
-    login_user
-    let!(:category) { create(:category) }
-
-    context 'with valid attributes' do
-      it 'allows to set all the passed attributes and redirects to post' do
-        put :update, params: { 
-          post: {
-            title: "New title",
-            content: "New content here",
-            category_id: category.id,
-            cover: nil,
-            published: "2019-07-08",
-            introduction: "New intro to post",
-            crop_x: nil, crop_y: nil, crop_width: nil, crop_height: nil,
-            crop_rectangle_x: nil, crop_rectangle_y: nil, crop_rectangle_width: nil, crop_rectangle_height: nil
-          },
-          id: published_post.id
-        }
-
-        expect(published_post.reload).to have_attributes(title: 'New title',
-        content: "New content here", category_id: category.id, cover: nil, introduction: 'New intro to post', crop_x: nil, crop_y: nil, crop_width: nil, crop_height: nil, crop_rectangle_x: nil, crop_rectangle_y: nil, crop_rectangle_width: nil, crop_rectangle_height: nil)
-
-        expect(response).to redirect_to(published_post)
-
+    context 'when user is logged_in' do
+      login_user
+      let!(:category) { create(:category) }
+  
+      context 'with valid attributes' do
+        it 'allows to set all the passed attributes and redirects to post' do
+          put :update, params: { 
+            post: {
+              title: "New title",
+              content: "New content here",
+              category_id: category.id,
+              cover: nil,
+              published: "2019-07-08",
+              introduction: "New intro to post",
+              crop_x: nil, crop_y: nil, crop_width: nil, crop_height: nil,
+              crop_rectangle_x: nil, crop_rectangle_y: nil, crop_rectangle_width: nil, crop_rectangle_height: nil
+            },
+            id: published_post.id
+          }
+  
+          expect(published_post.reload).to have_attributes(title: 'New title',
+          content: "New content here", category_id: category.id, cover: nil, introduction: 'New intro to post', crop_x: nil, crop_y: nil, crop_width: nil, crop_height: nil, crop_rectangle_x: nil, crop_rectangle_y: nil, crop_rectangle_width: nil, crop_rectangle_height: nil)
+  
+          expect(response).to redirect_to(published_post)
+        end
+      end
+  
+      context ' with invalid attributes' do
+        it 'does not chanages @photos\'s atrributes and redirect to the :edit template' do
+          put :update, params: {
+            post: {
+              title: nil,
+              cover: ''
+            },
+            id: published_post.id
+          }
+  
+          expect(published_post.reload.title).not_to eq(nil)
+          expect(response).to render_template('edit')
+        end
       end
     end
 
-    context ' with invalid attributes' do
-      it 'does not chanages @photos\'s atrributes and redirect to the :edit template' do
-        put :update, params: {
-          post: {
-            title: nil,
-            cover: ''
-          },
-          id: published_post.id
-        }
+    context 'when user is not logged_in' do
+      it 'redirects to user_session_path' do
+        post :update, params: { post: attributes_for(:post), id: published_post.id }
 
-        expect(published_post.reload.title).not_to eq(nil)
-        expect(response).to render_template('edit')
+        expect(response.status).to eq(302)
+        expect(response).to redirect_to(user_session_path)
       end
     end
   end
 
   describe 'DELETE #destroy' do
-    login_user
-
-    it 'deletes the post and returns a 302 status code for redirect' do
-      expect {
+    context 'when user is logged_in' do
+      login_user
+  
+      it 'deletes the post and returns a 302 status code for redirect' do
+        expect {
+          delete :destroy, params: { id: published_post.id }
+        }.to change(Post, :count).by(-1)
+  
+        expect(response.status).to eq(302)
+      end
+        
+      it 'redirects to posts#index' do
         delete :destroy, params: { id: published_post.id }
-      }.to change(Post, :count).by(-1)
-
-      expect(response.status).to eq(302)
+        expect(response).to redirect_to(root_path)
+      end
     end
+
+    context 'when user is not logged_in' do
+      it 'redirects to user_session_path' do
+        delete :destroy, params: { id: published_post.id }
       
-    it 'redirects to posts#index' do
-      delete :destroy, params: { id: published_post.id }
-      expect(response).to redirect_to(root_path)
+        expect(response.status).to eq(302)
+        expect(response).to redirect_to(user_session_path)
+      end
     end
   end
 end
