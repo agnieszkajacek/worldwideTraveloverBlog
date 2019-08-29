b091f8b94ade6f6871ca61b804bea46ee0ef4606
agnieszka <agnieszka.jacek90@gmail.com>
2019-08-29 18:25:32 +0200

    Add specs for Photo Model

---
+++ spec/models/photo_spec.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Post, type: :model do
+  let!(:category) { create(:category) }
+
+  context 'validations' do
+    it 'is valid with name and category_id' do
+      photo = Photo.create(name: 'test', category_id: category.id)
+
+      expect(photo).to be_valid
+    end
+
+    it 'is invalid without name' do
+      photo = Photo.create(name: nil, category_id: category.id)
+
+      expect(photo).not_to be_valid
+      expect(photo.errors.details[:name]).to include(error: :blank)
+    end
+
+    it 'is invalid without category_id' do
+      photo = Photo.create(name: nil, category_id: nil)
+
+      expect(photo).not_to be_valid
+      expect(photo.errors.details[:category_id]).to include(error: :blank)
+    end
+  end
+
+  context 'associations' do
+    it { should belong_to(:category).optional }
+  end
+end
